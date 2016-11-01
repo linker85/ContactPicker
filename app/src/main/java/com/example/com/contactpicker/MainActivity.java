ca59105d08eb0f511048e16384ccc7345e239c21
linker85 <linker85@hotmail.com>
2016-10-31 20:19:48 -0400

    contact picker

---
+++ app/src/main/java/com/example/com/contactpicker/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.com.contactpicker;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final int PICK_CONTACT = 2015;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void choose(View view) {
+        Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+        startActivityForResult(i, PICK_CONTACT);
+    }
+
+    /*
+    * After the user has chosen a contact, you need to parse the result returned from startActivityForResult for a phone number.
+    * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_CONTACT && resultCode == RESULT_OK) {
+            Uri contactUri = data.getData();
+            Cursor cursor = getContentResolver().query(contactUri, null, null, null, null);
+            cursor.moveToFirst();
+            int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            Log.d("phone number", cursor.getString(column));
+        }
+    }
+
+}
